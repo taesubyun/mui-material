d95c64f55185f371e0784fe6cde6ed5550a1f7be
Taesub Yun <taesubyun@icloud.com>
2022-01-23 17:19:10 +0900

    feat : Gird 컴포넌트 생성 (default:12size)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Article from './components/Article';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+// mui grid는 기본적으로 한 행을 12등분한다.
+// 12등분을 초과하면 다음 행으로 넘어간다.
+
 function App() {
     return (
         <Container fixed>
             <Header />
-            <Nav />
-            <Article />
+            <Grid container>
+                <Grid item xs='2'>
+                    <Nav />
+                </Grid>
+                <Grid item xs='10'>
+                    <Article />
+                </Grid>
+            </Grid>
         </Container>
     );
 }
