cb08a7bab66e28fa8a114b9bea40098d449776fb
Taesub Yun <taesubyun@icloud.com>
2022-01-24 18:46:48 +0900

    feat : Container 추가로 작업 마무리

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -8,57 +8,67 @@ import Typography from '@mui/material/Typography';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Signin = () => {
     return (
-        <Box
-            sx={{
-                marginTop: 8,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-            }}
-        >
-            <Avatar sx={{ m: 2, bgcolor: 'secondary.main' }}>
-                <LockOutlinedIcon />
-            </Avatar>
+        <Container component='main' maxWidth='lg'>
+            {/* xs, sm, md, lg, xl */}
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 2, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
 
-            <Typography component='h1' variant='h5' sx={{ mb: 2 }}>
-                Sign in
-            </Typography>
-            <TextField
-                name='email'
-                label='Email Address'
-                required
-                fullWidth
-                autoComplete='email'
-                autoFocus
-            />
-            <TextField
-                name='password'
-                label='Password'
-                type='password'
-                required
-                fullWidth
-                autoComplete='current-password'
-            />
-            <FormControlLabel
-                control={<Checkbox value='remember' color='primary' />}
-                label='Remember me'
-            />
+                <Typography component='h1' variant='h5' sx={{ mb: 2 }}>
+                    Sign in
+                </Typography>
+                <TextField
+                    name='email'
+                    label='Email Address'
+                    required
+                    fullWidth
+                    autoComplete='email'
+                    autoFocus
+                />
+                <TextField
+                    margin='normal'
+                    name='password'
+                    label='Password'
+                    type='password'
+                    required
+                    fullWidth
+                    autoComplete='current-password'
+                />
+                <FormControlLabel
+                    control={<Checkbox value='remember' color='primary' />}
+                    label='Remember me'
+                />
 
-            <Button type='submit' fullWidth variant='contained' sx={{ mt: 4 }}>
-                Sign in
-            </Button>
-            <Grid container>
-                <Grid item xs>
-                    <Link>비밀번호를 잊으셨습니까?</Link>
+                <Button
+                    type='submit'
+                    fullWidth
+                    variant='contained'
+                    sx={{ mt: 4, mb: 2 }}
+                >
+                    Sign in
+                </Button>
+                <Grid container>
+                    <Grid item xs>
+                        <Link>비밀번호를 잊으셨습니까?</Link>
+                    </Grid>
+                    <Grid item>
+                        <Link>무료로 가입하세요</Link>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Link>무료로 가입하세요</Link>
-                </Grid>
-            </Grid>
-        </Box>
+            </Box>
+        </Container>
     );
 };
 
