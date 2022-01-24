60c7df1abd2c8c59e6989ad02f9195c795f13c25
Taesub Yun <taesubyun@icloud.com>
2022-01-24 18:38:21 +0900

    feat : Grid, Typography 그리고 LockOutlinedIcon npm install 완료

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -2,10 +2,18 @@ import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 const Signin = () => {
     return (
         <div>
+            <LockOutlinedIcon />
+            <Typography component='h1' variant='h5' sx={{ mb: 2 }}>
+                Sign in
+            </Typography>
             <TextField
                 name='email'
                 label='Email Address'
@@ -26,10 +34,18 @@ const Signin = () => {
                 control={<Checkbox value='remember' color='primary' />}
                 label='Remember me'
             />
-            <Checkbox value='remember' color='primary' />
+
             <Button type='submit' fullWidth variant='contained' sx={{ mt: 4 }}>
                 Sign in
             </Button>
+            <Grid container>
+                <Grid item xs>
+                    <Link>비밀번호를 잊으셨습니까?</Link>
+                </Grid>
+                <Grid item>
+                    <Link>무료로 가입하세요</Link>
+                </Grid>
+            </Grid>
         </div>
     );
 };
