dedab2f93307f63340934cc544bdc7ace7dd881e
Taesub Yun <taesubyun@icloud.com>
2022-01-24 18:21:20 +0900

    feat : Signin컴포넌트 내의 Textfiled,Checkbox,Button 추가

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,9 +1,28 @@
 import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
 
 const Signin = () => {
     return (
         <div>
-            <TextField />
+            <TextField
+                name='email'
+                label='Email Address'
+                required
+                fullWidth
+                autoComplete='email'
+                autoFocus
+            />
+            <TextField
+                name='password'
+                label='Password'
+                type='password'
+                required
+                fullWidth
+                autoComplete='current-password'
+            />
+            <Checkbox value='remember' color='primary' />
+            <Button type='submit'>Sign in</Button>
         </div>
     );
 };
